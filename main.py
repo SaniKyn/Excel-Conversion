435d9772859af639bb0700737224725487156358
SaniKyn <gajdukevich.sani@mail.ru>
2022-07-02 15:50:47 +0300

    write_in_one_table

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import pandas as pd
 
 k = float(input("Введите курс в рублей:"))
-df = pd.DataFrame({'RusRub': [1000, 200123, 1234, 123]})
+df = pd.read_excel('Сonversion.xlsx', sheet_name='Sheet1')
 num = []
 for i in df['RusRub']:
     n = round(i * k, 2)
     num.append(n)
 
-df1 = pd.DataFrame({'BelRub': num})
-salary_sheets = {'Group1': df, 'Group2': df1}
-writer = pd.ExcelWriter('D:/python/ExcelPython/Сonversion.xlsx', engine='xlsxwriter')
-
-for sheet_num in salary_sheets.keys():
-    salary_sheets[sheet_num].to_excel(writer, sheet_name=sheet_num, index=False)
-
-writer.save()
+df['BelRub'] = num
+df.to_excel('D:/python/ExcelPython/Сonversion.xlsx', index=False)
